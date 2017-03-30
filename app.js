03550886e1e3db4925b1ce53772bd8a237e87f77
Yondon Fu <yondon.fu@gmail.com>
2017-03-30 19:49:09 -0400

    Express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
